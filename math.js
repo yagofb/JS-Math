4e74d7589da42d1e710dcecf27087a65eca95ac5
yagofb <yagofernandezblandon1@gmail.com>
2025-04-04 18:28:44 +0200

    Genera un número aleatorio entre 5 y 10 redondeando y muéstralo por consola. Para  este caso lo recomendado es utilizar la fórmula (Math.random()* (max-min)+min).

--- math.js
+++ math.js
@@ -2,3 +2,8 @@ let numeroAleatorio1 = Math.round(Math.random() * 100);
 console.log(numeroAleatorio1);
 
 console.log(Math.PI);
+
+let min = 5;
+let max = 10;
+let numeroAleatorio2 = Math.round(Math.random() * (max - min) + min);
+console.log(numeroAleatorio2);
